d204c0a2a540406a50f202c1a94e101fcfdde60c
dyl9874 <159584716+Syntax-Soldier@users.noreply.github.com>
2025-04-28 20:04:15 -0500

    added update movieController.js

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -9,3 +9,22 @@ exports.getAllMovies = (req, res) => {
     res.json(results);
   });
 };
+
+exports.updateMovie = (req, res) => {
+  const movieId = req.query.id;
+  const newTitle = req.query.title;
+
+  console.log('Update Request Received:', movieId, newTitle);
+
+  const query = `UPDATE movie SET title = '${newTitle}' WHERE id = ${movieId}`;
+
+  console.log('Executing Change', query);
+
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Query Error:', err);
+      return res.status(500).json({ error: 'Server Error', details: err.message });
+    }
+    res.json({ success: true, message: 'Movie updated!' });
+  });
+};
